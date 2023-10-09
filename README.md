a830873df4854d0e1a710cdc8ba6a7398e2882ee
sea2985 <savannah7321@gmail.com>
2023-10-09 12:45:14 -0400

    Cleaned up files and added commenting

--- README.md
+++ README.md
@@ -1,1 +1,59 @@
-# intrusion-detection-system
+# Intrusion Detection System
+![alt background](resources/background.png)
+
+## Description
+intrusion_detection_system.py uses misuse-based (signature-based) and anomaly-based detection to classify packet
+information from the NSL-KDD dataset. These were classified by using a decision tree, a supervised learning algorithm.
+
+## Project Structure
+```
+Intrusion Detection System
+│   README.md
+│   ...
+└───src
+    │
+    └───data
+    │   │   anomaly_training_data.csv*
+    │   │   misuse_training_data.csv*
+    │   │   test.csv
+    │   │   train.csv
+    │
+    └───src
+        │   decision_tree.py
+        │   main.py
+        │   preprocessing.py
+        │   requirements.txt        
+        │
+```
+
+## Getting Started
+
+### Dependencies
+* Python3
+* Colorama 
+* Termcolor
+* Pandas
+* Sklearn
+
+### Installing
+```
+# Clone this repository
+$ git clone https://github.com/sea7321/intrusion-detection-system.git
+
+# Go into the repository
+$ cd intrusion-detection-system
+
+# Install dependencies
+$ pip install -r requirements.txt
+```
+
+### Executing program
+
+```
+# Run the intrusion detection system
+$ python3 intrusion_detection_system.py
+```
+
+## Authors
+* Savannah Alfaro, sea2985@rit.edu
+* Aaron Kersten, amk9398@rit.edu
--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,3 +1,9 @@
+"""
+File: preprocessing.py
+Description: Preprocesses the NSL-KDD dataset
+"""
+
+# Third-Party Imports
 import pandas as pd
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from sklearn.compose import ColumnTransformer
@@ -5,35 +11,46 @@ from sklearn.pipeline import Pipeline
 
 
 def process(data, preprocessor, categorical_columns, numeric_columns, target_column):
-    # Extract and keep the target variable
+    """
+    One-hot encodes and normalizes the data.
+    :param data: (DataFrame) the dataset
+    :param preprocessor: (ColumnTransformer) the column transformers
+    :param categorical_columns: (Str[]) list of categorical columns in the dataset
+    :param numeric_columns: (Str[]) list of numeric columns in the dataset
+    :param target_column: (Str[]) the target column in the dataset
+    :return: (DataFrame) the resulting DataFrame
+    """
+    # extract and keep the target variable
     y = data[target_column]
 
-    # Apply preprocessing to the feature columns
+    # apply preprocessing to the feature columns
     X_transformed = preprocessor.fit_transform(data.drop(target_column, axis=1))
 
-    # Get the feature names after one-hot encoding
+    # get the feature names after one-hot encoding
     categorical_feature_names = preprocessor.named_transformers_['cat'].named_steps['onehot'].get_feature_names_out(
         input_features=categorical_columns)
 
-    # Combine the categorical feature names with numeric column names
+    # combine the categorical feature names with numeric column names
     feature_names = list(categorical_feature_names) + numeric_columns
 
-    # Convert the transformed array back to a DataFrame with appropriate column names
+    # convert the transformed array back to a DataFrame with appropriate column names
     transformed_df = pd.DataFrame(X_transformed, columns=feature_names)
 
-    # Concatenate the feature DataFrame with the target variable
+    # concatenate the feature DataFrame with the target variable
     return pd.concat([transformed_df, y], axis=1)
 
 
 def preprocess():
-    # Load the training and testing data
-    print("Loading in the training data (dataset 1)...")
+    """
+    Preprocesses the NSL-KDD dataset into two datasets: misuse-based and anomaly-based.
+    :return: None
+    """
+    # load the training and testing data
+    print("\tLoading in the datasets...")
     training_data = pd.read_csv('../data/train.csv')
-
-    print("Loading in the testing data (dataset 2)...")
     testing_data = pd.read_csv('../data/test.csv')
 
-    # Separate categorical, numerical, and target columns
+    # separate categorical, numerical, and target columns
     categorical_columns = ['protocol_type', 'service', 'flag']
     numeric_columns = ['duration', 'src_bytes', 'dst_bytes', 'land', 'wrong_fragment', 'urgent', 'hot',
                        'num_failed_logins', 'logged_in', 'num_compromised', 'root_shell', 'su_attempted', 'num_root',
@@ -45,16 +62,16 @@ def preprocess():
                        'dst_host_srv_serror_rate', 'dst_host_rerror_rate', 'dst_host_srv_rerror_rate']
     target_column = ['class']
 
-    # Create transformers for one-hot encoding and standard scaling
+    # create transformers for one-hot encoding and standard scaling
     categorical_transformer = Pipeline(steps=[
-        ('onehot', OneHotEncoder(sparse=False))
+        ('onehot', OneHotEncoder(sparse_output=False))
     ])
 
     numeric_transformer = Pipeline(steps=[
         ('scaler', StandardScaler())
     ])
 
-    # Combine transformers using ColumnTransformer for feature columns
+    # combine transformers using ColumnTransformer for feature columns
     preprocessor = ColumnTransformer(
         transformers=[
             ('cat', categorical_transformer, categorical_columns),
@@ -62,14 +79,12 @@ def preprocess():
         ], remainder='passthrough'  # This includes the target variable without transformation
     )
 
-    # Process the training and testing data
-    print("Processing the training data...")
+    # process the training and testing data
+    print("\tProcessing the datasets...")
     training_df = process(training_data, preprocessor, categorical_columns, numeric_columns, target_column)
-
-    print("Processing the testing data...")
     testing_df = process(testing_data, preprocessor, categorical_columns, numeric_columns, target_column)
 
-    # Fill columns with 0 if they don't exist in the other dataset
+    # fill columns with 0 if they don't exist in the other dataset
     for column in training_df.columns:
         if column not in testing_df:
             testing_df[column] = 0
@@ -78,12 +93,20 @@ def preprocess():
         if column not in training_df:
             training_df[column] = 0
 
-    # Reorder columns and concatenate both datasets
-    print("Reordering and combining datasets...")
+    # reorder columns and concatenate both datasets
+    print("\tReordering and combining datasets...")
     testing_df = testing_df[training_df.columns.tolist()]
-    result = pd.concat([training_df, testing_df])
+    misuse_df = pd.concat([training_df, testing_df])
 
-    # Send the resulting dataframe to a CSV file
-    print("Writing the data to a CSV file...")
-    result.to_csv(r'../data/training_data.csv', index=False)
-    print("Wrote the data to ./data/training_data.csv")
+    # create a copy of the DataFrame for misuse data
+    anomaly_df = misuse_df.copy()
+
+    # replace values other than "normal"
+    anomaly_df.loc[anomaly_df['class'] != 'normal', 'class'] = 'abnormal'
+
+    # send the resulting dataframes to a CSV file
+    print("\tWriting the data to a CSV file...")
+    misuse_df.to_csv(r'../data/misuse_training_data.csv', index=False)
+    print("\tWrote the data to ./data/misuse_training_data.csv")
+    anomaly_df.to_csv(r'../data/anomaly_training_data.csv', index=False)
+    print("\tWrote the data to ./data/anomaly_training_data.csv\n")
